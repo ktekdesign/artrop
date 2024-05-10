58f63d45c7e8fea9a966d1e09bd6b8aea006faf0
Christian Kpanou <contact@ktekdesign.com>
2024-05-10 00:09:26 -0300

    fix: dashboard operation route

--- app/api/dashboard/operations/[[...id]]/route.ts
+++ app/api/dashboard/operations/[[...id]]/route.ts
@@ -1,39 +1,25 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '../../../../../utils/client';
 
-// export async function GET() {
-//   const drivers = await prisma.travel.groupBy({
-//     by: 'userId',
-//     _avg: {
-//       duration: true
-//     }
-//   });
-
-//   return NextResponse.json(drivers);
-// }
-export async function GET() {
-  const operations = await prisma.operation.groupBy({
-    by: 'type',
-    _count: {
-      type: true
-    }
-  });
-  return NextResponse.json(operations);
-}
-
 export async function POST(req: NextRequest) {
-  const { interval } = await req.json();
-  const end = new Date();
-  const start = new Date(new Date().setDate(end.getDate() - interval));
+  const { interval, start, end } = await req.json();
+  const today = new Date(new Date().setDate(new Date().getDate() + 1));
+  const endAt = interval !== 'custom' ? today : new Date(end);
+  endAt.setHours(0, 0, 0, 0);
+  const startAt =
+    interval !== 'custom'
+      ? new Date(new Date().setDate(today.getDate() - interval))
+      : new Date(start);
+  startAt.setHours(0, 0, 0, 0);
 
   const whereClause =
     start && end
       ? {
           startedAt: {
-            gte: start
+            gte: startAt
           },
           endedAt: {
-            lte: end
+            lte: endAt
           }
         }
       : undefined;
